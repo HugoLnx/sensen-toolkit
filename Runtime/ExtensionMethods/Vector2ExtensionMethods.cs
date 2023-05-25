e32d01c182b8fa8dedf1bfa74b018f414288ddcd
hugolnx <hugolnx@gmail.com>
2023-05-25 11:37:17 -0300

    Extensions methods for Vector2 and Vector3

---
+++ Runtime/ExtensionMethods/Vector2ExtensionMethods.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace SensenToolkit
+{
+    public static class Vector2ExtensionMethods
+    {
+        public static Vector3 X0Y(this Vector2 v2)
+        {
+            return new Vector3(v2.x, 0, v2.y);
+        }
+
+        public static Vector2 RotateBy(this Vector2 vec, float angle)
+        {
+            float radians = angle * Mathf.Deg2Rad;
+            float cos = Mathf.Cos(radians);
+            float sin = Mathf.Sin(radians);
+            return new Vector2(
+                x: (vec.x * cos) - (vec.y * sin),
+                y: (vec.x * sin) + (vec.y * cos)
+            );
+        }
+
+        public static Vector2 RotateTowards(
+            this Vector2 current, Vector2 target,
+            float maxRadiansDelta = float.MaxValue,
+
+            // Preferred rotation direction when vectors are opposites
+            Vector2? preferredDirection = null)
+        {
+            float maxDelta = maxRadiansDelta * Mathf.Rad2Deg;
+            float targetAngle = Vector2.SignedAngle(current, target);
+            if (preferredDirection.HasValue && Mathf.Abs(targetAngle) >= 179.999f)
+            {
+                float preferredSign = Mathf.Sign(Vector2.SignedAngle(current, preferredDirection.Value));
+                targetAngle = preferredSign * 179.999f;
+            }
+            float angle = Mathf.MoveTowardsAngle(0f, targetAngle, maxDelta);
+            return current.RotateBy(angle);
+        }
+
+        public static Vector2 PerpendicularAntiClockwise(this Vector2 v)
+        {
+            return new Vector2(-v.y, v.x);
+        }
+
+        public static Vector2 PerpendicularClockwise(this Vector2 v)
+        {
+            return -PerpendicularAntiClockwise(v);
+        }
+
+        public static Vector2 AsLocalOf(this Vector2 vec, Vector2 refVect)
+        {
+            refVect = refVect.normalized;
+            return new Vector2(
+                x: Vector2.Dot(vec, refVect.PerpendicularAntiClockwise()),
+                y: Vector2.Dot(vec, refVect)
+            );
+        }
+    }
+}
---
+++ Runtime/ExtensionMethods/Vector3ExtensionMethods.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace SensenToolkit
+{
+    public static class Vector3ExtensionMethods
+    {
+        public static Vector2 XZ(this Vector3 v3)
+        {
+            return new Vector2(v3.x, v3.z);
+        }
+        public static Vector3 X0Z(this Vector3 v3)
+        {
+            return new Vector3(v3.x, 0, v3.z);
+        }
+        public static Vector3 XYZ(this Vector3 v3)
+        {
+            return new Vector3(v3.x, v3.y, v3.z);
+        }
+
+        public static Vector3 Clone(this Vector3 v3, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(
+                x ?? v3.x,
+                y ?? v3.y,
+                z ?? v3.z
+            );
+        }
+    }
+}
