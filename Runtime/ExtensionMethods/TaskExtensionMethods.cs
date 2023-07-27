7a635e9654e967d72431b81121a8598d98848814
hugolnx <hugolnx@gmail.com>
2023-07-27 15:57:55 -0300

    Add DelayWhileOffline and other auxiliary methods on TaskExtensions

--- Runtime/ExtensionMethods/TaskExtensionMethods.cs
+++ Runtime/ExtensionMethods/TaskExtensionMethods.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -11,38 +13,98 @@ namespace SensenToolkit
             return new WaitUntil(() => task.IsCompleted);
         }
 
-        public static IEnumerator WaitForCompletion<T>(this Task<T> task)
+        public static ConfiguredTaskAwaitable<T> AwaitInCurrentThread<T>(this Task<T> task)
         {
-            return new WaitUntil(() => task.IsCompleted);
+            return task.ConfigureAwait(true);
+        }
+        public static ConfiguredTaskAwaitable AwaitInCurrentThread(this Task task)
+        {
+            return task.ConfigureAwait(true);
+        }
+
+        public static ConfiguredTaskAwaitable<T> AwaitInAnyThread<T>(this Task<T> task)
+        {
+            return task.ConfigureAwait(false);
+        }
+        public static ConfiguredTaskAwaitable AwaitInAnyThread(this Task task)
+        {
+            return task.ConfigureAwait(false);
+        }
+
+        public static async Task<T> DelayWhile<T>(this Task<T> task, Func<bool> predicate)
+        {
+            while (predicate())
+            {
+                await Task.Delay(300).AwaitInAnyThread();
+            }
+            return await task.AwaitInCurrentThread();
+        }
+        public static async Task DelayWhile(this Task task, Func<bool> predicate)
+        {
+            await task
+            .ToTypedTask()
+            .DelayWhile<bool>(predicate)
+            .AwaitInCurrentThread();
+        }
+
+        public static async Task<T> DelayUntil<T>(this Task<T> task, Func<bool> predicate)
+        {
+            while (!predicate())
+            {
+                await Task.Delay(300).AwaitInAnyThread();
+            }
+            return await task.AwaitInCurrentThread();
+        }
+        public static async Task DelayUntil(this Task task, Func<bool> predicate)
+        {
+            await task
+            .ToTypedTask()
+            .DelayUntil<bool>(predicate)
+            .AwaitInCurrentThread();
         }
 
         public static async Task<T> RetryOnError<T>(this Task<T> task, int maxRetries = 3, float delay = 0.1f, float delayIncrement = 1f)
         {
-            try
+            for (int i = 0; i < maxRetries - 1; i++)
             {
-                return await task;
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e);
-                if (maxRetries > 0)
+                try
                 {
-                    await Task.Delay((int)(delay * 1000)).ConfigureAwait(false);
-                    return await task.RetryOnError(maxRetries - 1, delay + delayIncrement, delayIncrement + 1).ConfigureAwait(false);
+                    return await task.AwaitInCurrentThread();
                 }
-                else
+                catch (System.Exception e)
                 {
-                    throw;
+                    Debug.LogError(e);
+                    await Task.Delay((int)(delay * 1000)).AwaitInAnyThread();
+                    delay += delayIncrement;
                 }
             }
+
+            // Last try
+            await Task.Delay((int)(delay * 1000)).AwaitInAnyThread();
+            return await task.AwaitInCurrentThread();
         }
 
         public static async Task RetryOnError(this Task task, int maxRetries = 3, float delay = 0.1f, float delayIncrement = 1f)
         {
             await task
-            .ContinueWith(t => true)
-            .RetryOnError(maxRetries, delay, delayIncrement)
-            .ConfigureAwait(false);
+            .ToTypedTask()
+            .RetryOnError<bool>(maxRetries, delay, delayIncrement)
+            .AwaitInCurrentThread();
+        }
+        public static Task<T> DelayWhileOffline<T>(this Task<T> task)
+        {
+            return task.DelayWhile(() => Application.internetReachability == NetworkReachability.NotReachable);
+        }
+        public static Task DelayWhileOffline(this Task task)
+        {
+            return task
+            .ToTypedTask()
+            .DelayWhileOffline<bool>();
+        }
+
+        private static Task<bool> ToTypedTask(this Task task)
+        {
+            return task.ContinueWith(_ => true, TaskScheduler.FromCurrentSynchronizationContext());
         }
     }
 }
