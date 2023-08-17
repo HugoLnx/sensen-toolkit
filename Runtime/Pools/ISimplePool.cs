9606559fe8260a09759a30771b6294f219d2e414
hugolnx <hugolnx@gmail.com>
2023-08-17 13:53:19 -0300

    SimpleExpandablePool gets hold of it's creations and control how many were created

--- Runtime/Pools/ISimplePool.cs
+++ Runtime/Pools/ISimplePool.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace SensenToolkit.Pools
 {
     public interface ISimplePool<T>
     {
         T Get();
         void Release(T resource);
+        HashSet<T> Creations { get; }
     }
 }
--- Runtime/Pools/SimpleExpandablePool.cs
+++ Runtime/Pools/SimpleExpandablePool.cs
@@ -5,14 +5,17 @@ namespace SensenToolkit.Pools
 {
     public class SimpleExpandablePool<T> : ISimplePool<T>
     {
-        private Func<T> _factory;
-        private Queue<T> _resources = new();
-        private int _minSize;
+        private readonly Func<T> _factory;
+        private readonly Queue<T> _resources = new();
+        private readonly int _minSize;
+        private readonly int _maxCreations;
+        public HashSet<T> Creations { get; } = new();
 
-        public SimpleExpandablePool(Func<T> factory, int minSize = 20)
+        public SimpleExpandablePool(Func<T> factory, int minSize = 20, int? maxCreations = null)
         {
             _factory = factory;
             _minSize = minSize;
+            _maxCreations = maxCreations ?? minSize + 30;
 
             for (int i = 0; i < _minSize; i++)
             {
@@ -36,7 +39,13 @@ namespace SensenToolkit.Pools
 
         private void Grow()
         {
-            _resources.Enqueue(_factory());
+            if (Creations.Count >= _maxCreations)
+            {
+                throw new InvalidOperationException("Pool has reached max it should create");
+            }
+            T creation = _factory();
+            Creations.Add(creation);
+            _resources.Enqueue(creation);
         }
     }
 }
