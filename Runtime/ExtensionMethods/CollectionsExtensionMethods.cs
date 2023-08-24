9910a2a9c1a912079bc300a80cfadb3d15752a0e
hugolnx <hugolnx@gmail.com>
2023-08-24 16:53:37 -0300

    Extension methods to get IEnumerable by slices

--- Runtime/ExtensionMethods/CollectionsExtensionMethods.cs
+++ Runtime/ExtensionMethods/CollectionsExtensionMethods.cs
@@ -9,5 +9,29 @@ namespace SensenToolkit
         {
             return source.OrderBy(x => UnityEngine.Random.value);
         }
+
+        public static IEnumerable<IEnumerable<T>> SlicesOf<T>(this IEnumerable<T> source, int size)
+        {
+            List<T> list = new(size);
+            foreach (T item in source)
+            {
+                list.Add(item);
+                if (list.Count == size)
+                {
+                    yield return list;
+                    list = new(size);
+                }
+            }
+            if (list.Count > 0)
+            {
+                yield return list;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> SliceByAmount<T>(this IEnumerable<T> source, int amount)
+        {
+            int size = source.Count() / amount;
+            return source.SlicesOf(size);
+        }
     }
 }
