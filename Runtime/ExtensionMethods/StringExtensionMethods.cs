3c64de256f03937b75c066f05edd57490edfc467
hugolnx <hugolnx@gmail.com>
2023-08-24 16:54:03 -0300

    String extension method to transform in slug

--- Runtime/ExtensionMethods/StringExtensionMethods.cs
+++ Runtime/ExtensionMethods/StringExtensionMethods.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 namespace SensenToolkit
 {
     public static class StringExtensionMethods
     {
+        private static readonly Regex RegexNonSlugChars = new(@"[^\d\w-]", RegexOptions.Compiled);
+        private static readonly Regex RegexBlankChars = new(@"\s+", RegexOptions.Compiled);
         public static string Capitalize(this string str)
         {
             if (str == null || str.Length == 0) return str;
             return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        public static string ToSlug(this string str)
+        {
+            str = str.Trim().ToLowerInvariant();
+            str = RegexBlankChars.Replace(str, "-");
+            str = RegexNonSlugChars.Replace(str, "_");
+            return str;
         }
     }
 }
