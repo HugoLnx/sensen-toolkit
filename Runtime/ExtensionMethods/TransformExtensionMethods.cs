e3e30a9648527c87297a316362e34eaac93c1d41
hugolnx <hugolnx@gmail.com>
2023-08-29 08:33:25 -0300

    Transform.DestroyAllChildren can skip specific children

--- Runtime/ExtensionMethods/TransformExtensionMethods.cs
+++ Runtime/ExtensionMethods/TransformExtensionMethods.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace SensenToolkit
 {
     public static class TransformExtensionMethods
     {
-        public static void DestroyAllChildren(this Transform transform)
+        public static void DestroyAllChildren(this Transform transform, IEnumerable<Transform> except = null)
         {
+            HashSet<int> exceptIds = except != null
+                ? new HashSet<int>(except.Select(t => t.GetInstanceID()))
+                : null;
             foreach (Transform child in transform)
             {
+                if (exceptIds?.Contains(child.GetInstanceID()) == true)
+                {
+                    continue;
+                }
                 GameObject.Destroy(child.gameObject);
             }
         }
