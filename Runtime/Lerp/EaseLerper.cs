4bbe023a39b4f0b7a8dee5ef98af44d217d62a67
hugolnx <hugolnx@gmail.com>
2024-05-16 11:45:07 +0200

    Creates class to easily lerp using DOTWeen curves

---
+++ Runtime/Lerp/EaseLerper.cs
@@ -0,0 +1,55 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace SensenToolkit.Lerp
+{
+    public class EaseLerper
+    {
+        private float _value;
+        private Tween _tween;
+
+        public EaseLerper()
+        {
+            _tween = DOTween.To(
+                getter: () => _value,
+                setter: (x) => _value = x,
+                endValue: 1f,
+                duration: 1f
+            )
+            .From(0f)
+            .SetUpdate(UpdateType.Manual, isIndependentUpdate: true)
+            .SetAutoKill(false);
+        }
+
+        public float Lerp(float a, float b, float t, Ease ease = Ease.Linear)
+        {
+            float easedT = ApplyEase(t, ease);
+            return Mathf.Lerp(a, b, easedT);
+        }
+
+        public float LerpUnclamped(float a, float b, float t, Ease ease = Ease.Linear)
+        {
+            float easedT = ApplyEase(t, ease);
+            return Mathf.LerpUnclamped(a, b, easedT);
+        }
+
+        private float ApplyEase(float t, Ease ease)
+        {
+            return ease switch
+            {
+                Ease.Unset => t,
+                Ease.Linear => t,
+                _ => ApplyEaseWithTweener(t, ease)
+            };
+        }
+
+        private float ApplyEaseWithTweener(float t, Ease ease)
+        {
+            _tween.Restart();
+            _tween.SetEase(ease);
+            _tween.ManualUpdate(t, t);
+            return _value;
+        }
+    }
+}
---
+++ Runtime/Lerp/SpecificEaseLerper.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+
+namespace SensenToolkit.Lerp
+{
+    public class SpecificEaseLerper : ILerper<float>
+    {
+        private readonly Ease _ease;
+        private readonly EaseLerper _curve;
+
+        public SpecificEaseLerper(Ease ease)
+        {
+            _ease = ease;
+            _curve = new EaseLerper();
+        }
+
+        public float Lerp(float v1, float v2, float t)
+        {
+            return _curve.Lerp(v1, v2, t, _ease);
+        }
+
+        public float LerpUnclamped(float v1, float v2, float t)
+        {
+            return _curve.LerpUnclamped(v1, v2, t, _ease);
+        }
+    }
+}
