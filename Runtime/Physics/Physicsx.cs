874574183a0589cb533e32da66774677ad72e473
hugolnx <hugolnx@gmail.com>
2024-11-01 14:26:25 -0300

    Physicsx.RaycastWithOverlap do overlap check first and can receive objects to ignore

--- Runtime/Physics/Physicsx.cs
+++ Runtime/Physics/Physicsx.cs
@@ -1,50 +1,86 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SensenToolkit
 {
     public static class Physicsx
     {
-        private static Collider[] s_collidersBuffer = new Collider[1];
-        public static bool RaycastAndOverlap(
+        private static Collider[] s_collidersBuffer = new Collider[64];
+
+        public static bool RaycastWithOverlap(
             Ray ray,
             float maxDistance = Mathf.Infinity,
             LayerMask layerMask = default,
-            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal,
+            HashSet<GameObject> ignoreOverlapOnAll = null,
+            GameObject ignoreOverlapOnObj = null
         )
         {
-            return RaycastAndOverlap(ray, out _, maxDistance, layerMask, queryTriggerInteraction);
+            return RaycastWithOverlap(ray, out _, maxDistance, layerMask, queryTriggerInteraction, ignoreOverlapOnAll, ignoreOverlapOnObj);
         }
-        public static bool RaycastAndOverlap(
+        public static bool RaycastWithOverlap(
             Ray ray,
             out RaycastHitx hitInfo,
             float maxDistance = Mathf.Infinity,
             LayerMask layerMask = default,
-            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal,
+            HashSet<GameObject> ignoreOverlapOnAll = null,
+            GameObject ignoreOverlapOnObj = null
         )
         {
-            return RaycastAndOverlap(ray.origin, ray.direction, out hitInfo, maxDistance, layerMask, queryTriggerInteraction);
+            return RaycastWithOverlap(ray.origin, ray.direction, out hitInfo, maxDistance, layerMask, queryTriggerInteraction, ignoreOverlapOnAll, ignoreOverlapOnObj);
         }
-        public static bool RaycastAndOverlap(
+        public static bool RaycastWithOverlap(
             Vector3 origin,
             Vector3 direction,
             float maxDistance = Mathf.Infinity,
             LayerMask layerMask = default,
-            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal,
+            HashSet<GameObject> ignoreOverlapOnAll = null,
+            GameObject ignoreOverlapOnObj = null
         )
         {
-            return RaycastAndOverlap(origin, direction, out _, maxDistance, layerMask, queryTriggerInteraction);
+            return RaycastWithOverlap(origin, direction, out _, maxDistance, layerMask, queryTriggerInteraction, ignoreOverlapOnAll, ignoreOverlapOnObj);
         }
 
-        public static bool RaycastAndOverlap(
+        public static bool RaycastWithOverlap(
             Vector3 origin,
             Vector3 direction,
             out RaycastHitx hitInfo,
             float maxDistance = Mathf.Infinity,
             LayerMask layerMask = default,
-            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal
+            QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal,
+            HashSet<GameObject> ignoreOverlapOnAll = null,
+            GameObject ignoreOverlapOnObj = null
         )
         {
             hitInfo = default;
+
+            int count = Physics.OverlapSphereNonAlloc(
+                position: origin,
+                radius: 0.01f,
+                results: s_collidersBuffer,
+                layerMask: layerMask,
+                queryTriggerInteraction: queryTriggerInteraction
+            );
+            Collider overlaped = null;
+            for (int i = 0; i < count; i++)
+            {
+                GameObject obj = s_collidersBuffer[i].gameObject;
+                if (ignoreOverlapOnAll != null && ignoreOverlapOnAll.Contains(obj)) continue;
+                if (ignoreOverlapOnObj != null && obj == ignoreOverlapOnObj) continue;
+                overlaped = s_collidersBuffer[i];
+                break;
+            }
+            if (overlaped != null)
+            {
+                hitInfo.point = origin;
+                hitInfo.normal = direction.normalized;
+                hitInfo.distance = 0f;
+                hitInfo.collider = overlaped;
+                return true;
+            }
             if (Physics.Raycast(
                 origin,
                 direction,
@@ -57,14 +93,19 @@ namespace SensenToolkit
                 hitInfo = RaycastHitx.FromRaycastHit(hitInfoX);
                 return true;
             }
-            int count = Physics.OverlapSphereNonAlloc(origin, 0.1f, s_collidersBuffer, layerMask);
-            if (count == 0) return false;
 
-            hitInfo.point = origin;
-            hitInfo.normal = direction.normalized;
-            hitInfo.distance = 0f;
-            hitInfo.collider = s_collidersBuffer[0];
-            return true;
+            return false;
+        }
+
+        public static HashSet<GameObject> FindSelfColliderObjects(GameObject gameObject)
+        {
+            HashSet<GameObject> objs = new();
+            Collider[] colliders = gameObject.GetComponentsInChildren<Collider>(includeInactive: true);
+            foreach (Collider collider in colliders)
+            {
+                objs.Add(collider.gameObject);
+            }
+            return objs;
         }
     }
 }
